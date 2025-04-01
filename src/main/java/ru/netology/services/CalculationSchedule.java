d423f20db544ba343fe4ed4c605718485140717e
EkaterinaAsanova <119072908+EkaterinaAsanova@users.noreply.github.com>
2025-04-02 01:12:04 +0500

    Update CalculationSchedule.java

--- src/main/java/ru/netology/services/CalculationSchedule.java
+++ src/main/java/ru/netology/services/CalculationSchedule.java
@@ -4,7 +4,7 @@ public class CalculationSchedule {
     public int calculate(int income, int expenses, int threshold) {
         int count = 0; // счётчик месяцев отдыха
         int money = 0; // количество денег на счету
-        int traty;
+        int wastes;    //траты 
         for (int month = 1; month <= 12; month++) {
             if (money < threshold) { // можем ли отдыхать?
                 System.out.println("Месяц " + month + " Денег " + money +
@@ -16,12 +16,12 @@ public class CalculationSchedule {
 
             } else {
 
-                traty = (money - expenses) - ((money - expenses) / 3);
+                wastes = (money - expenses) - ((money - expenses) / 3);
 
                 System.out.println("Месяц " + month + " Денег " + money +
                         " Буду отдыхать. Потратил -" + expenses +
-                        " затем ещё -" + traty);
-                money = money - (traty + expenses);
+                        " затем ещё -" + wastes);
+                money = money - (wastes + expenses);
                 count++;
             }
         }
